2f9489f75f5f081f707320153ac077199c46d9e5
Ben Anderson <bda5161@psu.edu>
2024-02-08 17:24:01 -0500

    Update makeGCs.py
    Added updated mix step and decreased robot speed

--- makeGCs.py
+++ makeGCs.py
@@ -1,16 +1,13 @@
 from opentrons import protocol_api
-import csv
-import numpy
 
 # tweakable settings
-
 
 Culture_Volume = 0.8 # volume to load of each strain
 
 # metadata
 metadata = {
-	'protocolName': 'Growth Curve Setup', 
-	'author': 'S Tian, sbt5355@psu.edu',
+	'protocolName': 'Strain Collection 384 Well Growth Curve Setup', 
+	'author': 'J Bisanz and B Anderson, bda5161@psu.edu',
 	'description': 'Transfer strains from 96 well growth plate to 2x 384 plates with test compound pre-loaded. Tips returned to box to be discarded!',
 	'apiLevel': '2.7'
 }
@@ -26,11 +23,19 @@ def run(protocol: protocol_api.ProtocolContext):
 	# define pipettes
 	#p20 = protocol.load_instrument('p20_single_gen2', 'left', tip_racks=[tips1])
 	multi20 = protocol.load_instrument('p20_multi_gen2', 'right', tip_racks=[tips1])
+
+	# decrease pipette speed to reduce risk of cross-contamination
+	multi20.default_speed = 100
 	
-	
+
 	for i in range(1, 13):
 		multi20.pick_up_tip()
-		multi20.mix(5, 20) # mix culture before taking out
+
+		for j in range(1,3): # loops through mixing 2 times (12 total mixes)
+			multi20.mix(2, 20,cultureplate['A'+str(i)]) # mix culture before taking out
+			multi20.mix(2, 20,cultureplate['A'+str(i)].bottom(z=4)) # Mix at top of well
+			multi20.mix(2, 20,cultureplate['A'+str(i)]) # final mix at normal height.
+			
 		multi20.aspirate(Culture_Volume*9, cultureplate['A'+str(i)]) # pull one extra load dead volume
 		multi20.dispense(Culture_Volume, assayplate1['A'+str(i*2-1)])
 		multi20.dispense(Culture_Volume, assayplate1['A'+str(i*2)])
